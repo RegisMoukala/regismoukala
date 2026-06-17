854560d54e1af6de4ee39014b504e71ef85ae684
RegisMoukala <regismoukala6@gmail.com>
2026-06-17 11:50:45 +0100

    Create home page

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Hero from '@/components/sections/Hero'
+import Services from '@/components/sections/Services'
+import Testimonials from '@/components/sections/Testimonials'
+import CTA from '@/components/sections/CTA'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Testimonials />
+      <CTA />
+    </>
+  )
+}
